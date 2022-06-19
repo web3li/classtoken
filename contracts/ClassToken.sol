4a48d53b247de0bfa2ff875ac9b3704363f2497a
web3li <zjlining@qq.com>
2022-06-19 17:18:07 +0800

    update contract ClassToken

--- contracts/ClassToken.sol
+++ contracts/ClassToken.sol
@@ -4,7 +4,5 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract ClassToken is ERC20 {
-    constructor() ERC20("ClassToken", "CLT") {
-        _mint(msg.sender, 10000 * 1e18);
-    }
+    constructor() ERC20("ClassToken", "CLT") {}
 }
